d3791f24e6f527100ddf1069f77d9286e7ff36f9
rexykilller441-maker <rexykilller441@gmail.com>
2025-10-22 22:47:39 +0530

    Update AnchorMacroClient.java
    Updated AnchorMacroClient.java source code.

--- src/main/java/com/rexykiller/anchormacro/AnchorMacroClient.java
+++ src/main/java/com/rexykiller/anchormacro/AnchorMacroClient.java
@@ -1,1 +1,29 @@
+package com.rexykiller.anchormacro;
 
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class AnchorMacroClient implements ClientModInitializer {
+    private static KeyBinding activateKey;
+
+    @Override
+    public void onInitializeClient() {
+        AnchorMacroConfig.load();
+        activateKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.anchormacro.activate",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_R,
+                "category.anchormacro"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (activateKey.wasPressed()) {
+                AnchorMacroExecutor.execute(client);
+            }
+        });
+    }
+}
