700a08f2e005879428a8ec50db74920a400fb3a8
rexykilller441-maker <rexykilller441@gmail.com>
2025-10-22 23:30:02 +0530

    Update AnchorMacroClient.java
    Updated AnchorMacroClient.java source code

--- src/main/java/com/rexykiller/anchormacro/AnchorMacroClient.java
+++ src/main/java/com/rexykiller/anchormacro/AnchorMacroClient.java
@@ -1,5 +1,6 @@
 package com.rexykiller.anchormacro;
 
+import com.rexykiller.anchormacro.client.AnchorMacroConfigScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -8,11 +9,15 @@ import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class AnchorMacroClient implements ClientModInitializer {
+
     private static KeyBinding activateKey;
+    private static KeyBinding configKey;
 
     @Override
     public void onInitializeClient() {
         AnchorMacroConfig.load();
+
+        // Macro activation key (default R)
         activateKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.anchormacro.activate",
                 InputUtil.Type.KEYSYM,
@@ -20,9 +25,24 @@ public class AnchorMacroClient implements ClientModInitializer {
                 "category.anchormacro"
         ));
 
+        // Config GUI key (default O)
+        configKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.anchormacro.config",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_O,
+                "category.anchormacro"
+        ));
+
+        // Tick event
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // Execute macro
             while (activateKey.wasPressed()) {
                 AnchorMacroExecutor.execute(client);
+            }
+
+            // Open config GUI
+            while (configKey.wasPressed()) {
+                client.setScreen(new AnchorMacroConfigScreen(client.currentScreen));
             }
         });
     }
